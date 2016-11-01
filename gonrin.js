178dd8f86eee46a53a6e7ed37e631dcae5144518
cuongnc <cuongnc.coder@gmail.com>
2016-11-01 11:33:19 +0700

    update DiaLogView

--- gonrin.js
+++ gonrin.js
@@ -955,17 +955,6 @@
 							    	uicontrol = field.uicontrol || "textbox";
 							    	break;
 							    	
-							    //TODO: remove ref type
-							    /*case "ref":
-							    	//load entity
-							    	var reflink = _.result(model_schema, field.field) || {};
-							    	field.context = this.view;
-							    	field.textField = reflink["refTextField"] || null;
-							    	field.valueField = reflink["refValueField"] || null;
-							    	field.dataSource = reflink["$ref"];
-							    	uicontrol = "gonrinref";
-							        break;*/
-							    
 							    default:
 							        
 							}
@@ -1020,18 +1009,17 @@
 						switch(field.type) {
 							case "ref":
 								var reflink = _.result(modelSchema, field.field) || {}; 
-								uicontrol = field.uicontrol ||"gonrinref";
-								if(uicontrol === "gonrinref"){
+								uicontrol = field.uicontrol ||"ref";
+								if(uicontrol === "ref"){
 									field.context = this.view;
-									field.textField = reflink["refTextField"] || null;
-							    	field.valueField = reflink["refValueField"] || null;
-							    	field.dataSource = reflink["$ref"];
-							    	field.selectionMode = reflink["refHas"] === "many"? "multiple": "single";
+									//field.textField = reflink["refTextField"] || null;
+							    	//field.valueField = reflink["refValueField"] || null;
+							    	//field.dataSource = reflink["$ref"];
+							    	//field.selectionMode = reflink["refHas"] === "many"? "multiple": "single";
+								
 								};
 								
 								if(uicontrol === "grid"){
-									//field.textField = reflink["refTextField"] || null;
-							    	//field.valueField = reflink["refValueField"] || null;
 							    	field.dataSource = this.view.model.get(bind_attr) ||[];
 							    	field.context = this.view;
 							    	//field.selectionMode = reflink["refHas"] === "many"? "multiple": "single";
@@ -2024,7 +2012,27 @@
 			}
 		},
 		dialog:function(){
-			
+			if(gonrin.dialog){
+				gonrin.dialog.dialog.apply(this,arguments);
+			}
+		},
+		alert:function(){
+			if(gonrin.dialog){
+				gonrin.dialog.alert.apply(this,arguments);
+			}
+		},
+		confirm:function(){
+			if(gonrin.dialog){
+				gonrin.dialog.confirm.apply(this,arguments);
+			}
+		},
+		loading:function(show){
+			if(show === false){
+				//hide loading bar
+				
+			}else{
+				
+			}
 		},
 	});
 	
@@ -2107,275 +2115,9 @@
 
 	//Gonrin.Dialog`
 	
-	var templates = {
-			dialog:
-				"<div class='bootbox modal' tabindex='-1' role='dialog' style='z-index:9999'>" +
-				"<div class='modal-dialog'>" +
-				"<div class='modal-content'>" +
-				"<div class='modal-body'><div class='bootbox-body'></div></div>" +
-				"</div>" +
-				"</div>" +
-				"</div>",
-			header:
-				"<div class='modal-header'>" +
-				"<h4 class='modal-title'></h4>" +
-				"</div>",
-			footer:
-				"<div class='modal-footer'></div>",
-			closeButton:
-				"<button type='button' class='bootbox-close-button close' data-dismiss='modal' aria-hidden='true'>&times;</button>",
-			form:
-				"<form class='bootbox-form'></form>",
-			inputs: {
-				text:
-					"<input class='bootbox-input bootbox-input-text form-control' autocomplete=off type=text />",
-				textarea:
-					"<textarea class='bootbox-input bootbox-input-textarea form-control'></textarea>",
-				email:
-					"<input class='bootbox-input bootbox-input-email form-control' autocomplete='off' type='email' />",
-				select:
-					"<select class='bootbox-input bootbox-input-select form-control'></select>",
-				checkbox:
-					"<div class='checkbox'><label><input class='bootbox-input bootbox-input-checkbox' type='checkbox' /></label></div>",
-				date:
-					"<input class='bootbox-input bootbox-input-date form-control' autocomplete=off type='date' />",
-				time:
-					"<input class='bootbox-input bootbox-input-time form-control' autocomplete=off type='time' />",
-				number:
-					"<input class='bootbox-input bootbox-input-number form-control' autocomplete=off type='number' />",
-				password:
-					"<input class='bootbox-input bootbox-input-password form-control' autocomplete='off' type='password' />"
-			}
-		};
-	    
-	    var defaults = {
-				// default language
-				locale: "en",
-				// show backdrop or not. Default to static so user has to interact with dialog
-				backdrop: "static",
-				// animate the modal in/out
-				animate: true,
-				// additional class string applied to the top level dialog
-				className: null,
-				// whether or not to include a close button
-				closeButton: true,
-				// show the dialog immediately by default
-				show: true,
-				// dialog container
-				container: "body"
-		};
-	    
-	    /**
-		 * @private
-		 */
-		function _t(key) {
-			var locale = locales[defaults.locale];
-			return locale ? locale[key] : locales.en[key];
-		}
-
-		function processCallback(e, dialog, callback) {
-			e.stopPropagation();
-			e.preventDefault();
-
-			// by default we assume a callback will get rid of the dialog,
-			// although it is given the opportunity to override this
-
-			// so, if the callback can be invoked and it *explicitly returns false*
-			// then we'll set a flag to keep the dialog active...
-			var preserveDialog = $.isFunction(callback) && callback.call(dialog, e) === false;
-
-			// ... otherwise we'll bin it
-			if (!preserveDialog) {
-				dialog.modal("hide");
-			}
-		}
-
-		function getKeyLength(obj) {
-			// @TODO defer to Object.keys(x).length if available?
-			var k, t = 0;
-			for (k in obj) {
-				t ++;
-			}
-			return t;
-		}
-
-		function each(collection, iterator) {
-			var index = 0;
-			$.each(collection, function(key, value) {
-				iterator(key, value, index++);
-			});
-		}
-
-		function sanitize(options) {
-			var buttons;
-			var total;
-
-			/*if (typeof options !== "object") {
-				throw new Error("Please supply an object of options");
-			}
-
-			if (!options.message) {
-				throw new Error("Please specify a message");
-			}*/
-
-			// make sure any supplied options take precedence over defaults
-			options = $.extend({}, defaults, options);
-
-			if (!options.buttons) {
-				options.buttons = {};
-			}
-
-			buttons = options.buttons;
-
-			total = getKeyLength(buttons);
-
-			each(buttons, function(key, button, index) {
-
-				if ($.isFunction(button)) {
-					// short form, assume value is our callback. Since button
-					// isn't an object it isn't a reference either so re-assign it
-					button = buttons[key] = {
-							callback: button
-					};
-				}
-
-				// before any further checks make sure by now button is the correct type
-				if ($.type(button) !== "object") {
-					throw new Error("button with key " + key + " must be an object");
-				}
-
-				if (!button.label) {
-					// the lack of an explicit label means we'll assume the key is good enough
-					button.label = key;
-				}
-
-				if (!button.className) {
-					if (total <= 2 && index === total-1) {
-						// always add a primary to the main option in a two-button dialog
-						button.className = "btn-primary";
-					} else {
-						button.className = "btn-default";
-					}
-				}
-			});
-
-			return options;
-		}
-		
-		/**
-		 * map a flexible set of arguments into a single returned object
-		 * if args.length is already one just return it, otherwise
-		 * use the properties argument to map the unnamed args to
-		 * object properties
-		 * so in the latter case:
-		 * mapArguments(["foo", $.noop], ["message", "callback"])
-		 * -> { message: "foo", callback: $.noop }
-		 */
-		function mapArguments(args, properties) {
-			var argn = args.length;
-			var options = {};
-
-			if (argn < 1 || argn > 2) {
-				throw new Error("Invalid argument length");
-			}
-
-			if (argn === 2 || typeof args[0] === "string") {
-				options[properties[0]] = args[0];
-				options[properties[1]] = args[1];
-			} else {
-				options = args[0];
-			}
-
-			return options;
-		}
-
-		/**
-		 * merge a set of default dialog options with user supplied arguments
-		 */
-		function mergeArguments(defaults, args, properties) {
-			return $.extend(
-					// deep merge
-					true,
-					// ensure the target is an empty, unreferenced object
-					{},
-					// the base options object for this type of dialog (often just buttons)
-					defaults,
-					// args could be an object or array; if it's an array properties will
-					// map it to a proper options object
-					mapArguments(
-							args,
-							properties
-					)
-			);
-		}
-
-		/**
-		 * this entry-level method makes heavy use of composition to take a simple
-		 * range of inputs and return valid options suitable for passing to bootbox.dialog
-		 */
-		function mergeDialogOptions(className, labels, properties, args) {
-			//  build up a base set of dialog properties
-			var baseOptions = {
-					className: "bootbox-" + className,
-					buttons: createLabels.apply(null, labels)
-			};
-
-			// ensure the buttons properties generated, *after* merging
-			// with user args are still valid against the supplied labels
-			return validateButtons(
-					// merge the generated base properties with user supplied arguments
-					mergeArguments(
-							baseOptions,
-							args,
-							// if args.length > 1, properties specify how each arg maps to an object key
-							properties
-					),
-					labels
-			);
-		}
-
-		/**
-		 * from a given list of arguments return a suitable object of button labels
-		 * all this does is normalise the given labels and translate them where possible
-		 * e.g. "ok", "confirm" -> { ok: "OK, cancel: "Annuleren" }
-		 */
-		function createLabels() {
-			var buttons = {};
-
-			for (var i = 0, j = arguments.length; i < j; i++) {
-				var argument = arguments[i];
-				var key = argument.toLowerCase();
-				var value = argument.toUpperCase();
-
-				buttons[key] = {
-						label: _t(value)
-				};
-			}
-
-			return buttons;
-		}
-
-		function validateButtons(options, buttons) {
-			var allowedButtons = {};
-			each(buttons, function(key, value) {
-				allowedButtons[value] = true;
-			});
-
-			each(options.buttons, function(key) {
-				if (allowedButtons[key] === undefined) {
-					throw new Error("button key " + key + " is not allowed (options are " + buttons.join("\n") + ")");
-				}
-			});
-
-			return options;
-		}
-
 	Gonrin.DialogView = Gonrin.CollectionView.extend({
-    	selectedItems : [],
-    	selectionMode: "single",
     	tools : null,
     	$dialog: null,
-    	
     	//TODO: remove initModel.
     	initModel: function(){
 	       	this.collection = new Gonrin.Collection(Gonrin.Model);
@@ -2402,28 +2144,13 @@
     		}
     	},
     	
+    	
     	dialog: function(options){
     		var self = this;
-    		options = sanitize(options);
-   
-			this.$dialog = $(templates.dialog);
-			var innerDialog = this.$dialog.find(".modal-dialog");
-			var body = this.$dialog.find(".modal-body");
-			var buttons = options.buttons;
-			var buttonStr = "";
-			var callbacks = {
-					escape: options.escape,
-					success: options.success
-			};
-			if ($.fn.modal === undefined) {
-				throw new Error(
-						"$.fn.modal is not defined; please double check you have included " +
-						"the Bootstrap JavaScript library. See http://getbootstrap.com/javascript/ " +
-						"for more details."
-				);
-			};
-
-
+    		this.applyBindings();
+    		self.$dialog = gonrin.dialog.dialog({message:self.$el});
+    		
+    		
 			/*each(buttons, function(key, button) {
 
 				// @TODO I don't like this string appending to itself; bit dirty. Needs reworking
@@ -2434,7 +2161,7 @@
 			});*/
 			
 			
-			var selectBtn = $('<button/>').attr({type:'button', class:'btn btn-success btn-sm', 'btn-name':'select'}).html(self.getApp().translate('app.lang.select'));
+			/*var selectBtn = $('<button/>').attr({type:'button', class:'btn btn-success btn-sm', 'btn-name':'select'}).html(self.getApp().translate('app.lang.select'));
 			selectBtn.on("click", function(e) {
 				processCallback(e, self.$dialog, callbacks.success);
 			});
@@ -2488,114 +2215,8 @@
 			});
 			this.$el.find("[" + self.bindingBlocks + "=toolbar]").append(self.toolbar).after(self.progressbar);
 			
-			
 			//end toolbar
-			this.applyBindings();
-  
-			if (options.animate === true) {
-				self.$dialog.addClass("fade");
-			}
-
-			if (options.className) {
-				self.$dialog.addClass(options.className);
-			}
-
-			if (options.size === "large") {
-				innerDialog.addClass("modal-lg");
-			} else if (options.size === "small") {
-				innerDialog.addClass("modal-sm");
-			}
-
-			if (options.title) {
-				body.before(templates.header);
-			}
-
-			/*if (options.closeButton) {
-				var closeButton = $(templates.closeButton);
-
-				if (options.title) {
-					dialog.find(".modal-header").prepend(closeButton);
-				} else {
-					closeButton.css("margin-top", "-10px").prependTo(body);
-				}
-			}*/
-
-			if (options.title) {
-				self.$dialog.find(".modal-title").html(options.title);
-			}
-
-			if (buttonStr.length) {
-				body.after(templates.footer);
-				self.$dialog.find(".modal-footer").html(buttonStr);
-			}
-
-
-			/**
-			 * Bootstrap event listeners; used handle extra
-			 * setup & teardown required after the underlying
-			 * modal has performed certain actions
-			 */
-
-			self.$dialog.on("hidden.bs.modal", function(e) {
-				// ensure we don't accidentally intercept hidden events triggered
-				// by children of the current dialog. We shouldn't anymore now BS
-				// namespaces its events; but still worth doing
-				if (e.target === this) {
-					self.$dialog.remove();
-				}
-			});
-
-
-			self.$dialog.on("shown.bs.modal", function() {
-				self.$dialog.find(".btn-primary:first").focus();
-			});
-
-			self.$dialog.on("escape.close.bb", function(e) {
-				if (callbacks.escape) {
-					processCallback(e, self.$dialog, callbacks.escape);
-				}
-			});
-
-			/**
-			 * Standard jQuery event listeners; used to handle user
-			 * interaction with our dialog
-			 */
-
-			self.$dialog.on("click", ".modal-footer button", function(e) {
-				var callbackKey = $(this).data("bb-handler");
-
-				processCallback(e, self.$dialog, callbacks[callbackKey]);
-			});
-
-			/*dialog.on("click", ".bootbox-close-button", function(e) {
-				// escape might be falsy but that's fine; the fact is
-				// if the user has managed to click the close button we
-				// have to close the dialog, callback or not
-				processCallback(e, dialog, callbacks.escape);
-			});*/
-
-			self.$dialog.on("keyup", function(e) {
-				if (e.which === 27) {
-					self.$dialog.trigger("escape.close.bb");
-				}
-			});
-
-			// the remainder of this method simply deals with adding our
-			// dialogent to the DOM, augmenting it with Bootstrap's modal
-			// functionality and then giving the resulting object back
-			// to our caller
-
-			$(options.container).append(self.$dialog);
-
-			self.$dialog.modal({
-				backdrop: options.backdrop ? "static": false,
-						keyboard: false,
-						show: false
-			});
-
-			if (options.show) {
-				self.$dialog.modal("show");
-			}
+			this.applyBindings();*/
     		return this;
     	}
     });
